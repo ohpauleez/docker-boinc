54c298af9db8dd92d09333908dcb35dcfced9f32
Alex Olieman <alex@olieman.net>
2017-12-20 21:19:32 +0100

    Only reconfigure boinc-client when the data volume is empty

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,16 +3,25 @@ set -eo pipefail
 
 # if command starts with an option, prepend boinc 
 if [ "${1:0:1}" = '-' ]; then
-	set -- boinc "$@"
+    set -- boinc "$@"
 fi
 
+DATA_PATH=/var/lib/boinc-client
+
 if [ "$1" = 'boinc' ]; then
-    #work around for activity checking which sends errors to stderr if this directory doesn't exist
+    # work around for activity checking which sends errors to stderr if this directory doesn't exist
     mkdir -p /dev/input/mice
-    #fix permissions
-    chown -R boinc:boinc /var/lib/boinc-client
-    #reconfigure boinc just in case /var/lib/boinc-client is a volume and hasn't been used before
-    dpkg-reconfigure boinc-client
+    # fix permissions
+    chown -R boinc:boinc $DATA_PATH
+    
+    if [ ! "$(ls -A $DATA_PATH)" ]
+    then
+        # reconfigure boinc when the data directory is empty
+        dpkg-reconfigure boinc-client
+    else
+        echo "Existing configuration was found at $DATA_PATH. Not touching."
+    fi
+    
     exec gosu boinc "$@"
 else
     exec "$@"
